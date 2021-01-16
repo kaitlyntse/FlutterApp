e619cdd1268e79ef8689e247efd686072dfb96e3
Dominic Austria <dgaustri@uwaterloo.ca>
2021-01-16 17:15:01 -0500

    seperate pages into two files: checklistEditor and checklistReader

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+import 'checklistReader.dart';
+
 class Dashboard extends StatefulWidget {
   Dashboard({Key key, this.title}) : super(key: key);
 
@@ -21,6 +23,7 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   final FirebaseMessaging _messaging = FirebaseMessaging();
+  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -31,28 +34,21 @@ class _DashboardState extends State<Dashboard> {
     });
   }
 
-  int _counter = 0;
+  Widget getPage(int index) {
+    if (index == 0) {
+      return DynamicallyCheckbox();
+    } else if (index == 1) {
+      return null;
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    int _selectedIndex = 0;
+
     return new Scaffold(
       appBar: AppBar(
         title: const Text('Sample Code'),
@@ -88,75 +84,10 @@ class _DashboardState extends State<Dashboard> {
           VerticalDivider(thickness: 1, width: 1),
           // This is the main content.
           Expanded(
-            child: Center(
-              child: DynamicallyCheckbox(),
-            ),
+            child: Center(child: getPage(_selectedIndex)),
           )
         ],
       ),
     );
-  }
-}
-
-class DynamicallyCheckbox extends StatefulWidget {
-  @override
-  DynamicallyCheckboxState createState() => new DynamicallyCheckboxState();
-}
-
-class DynamicallyCheckboxState extends State {
-  Map<String, bool> List = {
-    'Wake Up': false,
-    'Eat Breakfast': false,
-    'Eat Lunch': false,
-    'Eat Dinner': false,
-    'Go to Sleep': false,
-  };
-
-  var holder_1 = [];
-
-  getItems() {
-    List.forEach((key, value) {
-      if (value == true) {
-        holder_1.add(key);
-      }
-    });
-
-    // Printing all selected items on Terminal screen.
-    print(holder_1);
-    // Here you will get all your selected Checkbox items.
-
-    // Clear array after use.
-    holder_1.clear();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      RaisedButton(
-        child: Text(" Get Checked Checkbox Values "),
-        onPressed: getItems,
-        color: Colors.pink,
-        textColor: Colors.white,
-        splashColor: Colors.grey,
-        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-      ),
-      Expanded(
-        child: ListView(
-          children: List.keys.map((String key) {
-            return new CheckboxListTile(
-              title: new Text(key),
-              value: List[key],
-              activeColor: Colors.deepPurple[400],
-              checkColor: Colors.white,
-              onChanged: (bool value) {
-                setState(() {
-                  List[key] = value;
-                });
-              },
-            );
-          }).toList(),
-        ),
-      ),
-    ]);
   }
 }
