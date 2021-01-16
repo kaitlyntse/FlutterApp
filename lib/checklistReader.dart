e6a590d3c6ac22c43961e37fecabc10a09e79a90
Dominic Austria <dgaustri@uwaterloo.ca>
2021-01-16 17:19:02 -0500

    created seperate file for tasks data

---
+++ lib/checklistReader.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'tasks.dart';
+
+class DynamicallyCheckbox extends StatefulWidget {
+  @override
+  DynamicallyCheckboxState createState() => new DynamicallyCheckboxState();
+}
+
+class DynamicallyCheckboxState extends State {
+  Map<String, bool> List = tasks;
+
+  var holder_1 = [];
+
+  getItems() {
+    List.forEach((key, value) {
+      if (value == true) {
+        holder_1.add(key);
+      }
+    });
+
+    // Printing all selected items on Terminal screen.
+    print(holder_1);
+    // Here you will get all your selected Checkbox items.
+
+    // Clear array after use.
+    holder_1.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      RaisedButton(
+        child: Text(" Get Checked Checkbox Values "),
+        onPressed: getItems,
+        color: Colors.pink,
+        textColor: Colors.white,
+        splashColor: Colors.grey,
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      ),
+      Expanded(
+        child: ListView(
+          children: List.keys.map((String key) {
+            return new CheckboxListTile(
+              title: new Text(key),
+              value: List[key],
+              activeColor: Colors.deepPurple[400],
+              checkColor: Colors.white,
+              onChanged: (bool value) {
+                setState(() {
+                  List[key] = value;
+                });
+              },
+            );
+          }).toList(),
+        ),
+      ),
+    ]);
+  }
+}
---
+++ lib/tasks.dart
@@ -0,0 +1,7 @@
+Map<String, bool> tasks = {
+  'Wake Up': false,
+  'Eat Breakfast': false,
+  'Eat Lunch': false,
+  'Eat Dinner': false,
+  'Go to Sleep': false,
+};
