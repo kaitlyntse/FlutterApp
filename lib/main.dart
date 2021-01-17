3109569d22e2a636187cc62dfeaeecf22ef7e217
mnabedin <mnabedin@uwaterloo.ca>
2021-01-16 19:03:28 -0500

    color changer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'dashboard.dart';
+import 'dart:async';
 import 'package:confetti/confetti.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  var bgColor;
   @override
   Widget build(BuildContext context) {
+    if (DateTime.now().hour >= 8 && DateTime.now().hour <= 12) {
+      bgColor = Colors.lightBlue;
+    } else if (DateTime.now().hour >= 13 && DateTime.now().hour <= 16) {
+      bgColor = Colors.deepOrange;
+    } else {
+      bgColor = Colors.indigo;
+    }
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
@@ -19,7 +28,7 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: bgColor,
       ),
       home: new Dashboard(title: 'Flutter Demo Home Page'),
     );
