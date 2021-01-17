218614c5ecf7052de2206d2e5f5cf5c8d1c15c70
mnabedin <mnabedin@uwaterloo.ca>
2021-01-16 20:27:17 -0500

    remove nav, add padding

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -55,36 +55,38 @@ class _DashboardState extends State<Dashboard> {
       ),
       body: Row(
         children: <Widget>[
-          NavigationRail(
-            selectedIndex: _selectedIndex,
-            onDestinationSelected: (int index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            labelType: NavigationRailLabelType.selected,
-            destinations: [
-              NavigationRailDestination(
-                icon: Icon(Icons.favorite_border),
-                selectedIcon: Icon(Icons.favorite),
-                label: Text('Home'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.bookmark_border),
-                selectedIcon: Icon(Icons.book),
-                label: Text('Second'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.star_border),
-                selectedIcon: Icon(Icons.star),
-                label: Text('Third'),
-              ),
-            ],
-          ),
-          VerticalDivider(thickness: 1, width: 1),
+          // NavigationRail(
+          //   selectedIndex: _selectedIndex,
+          //   onDestinationSelected: (int index) {
+          //     setState(() {
+          //       _selectedIndex = index;
+          //     });
+          //   },
+          //   labelType: NavigationRailLabelType.selected,
+          //   destinations: [
+          //     NavigationRailDestination(
+          //       icon: Icon(Icons.favorite_border),
+          //       selectedIcon: Icon(Icons.favorite),
+          //       label: Text('Home'),
+          //     ),
+          //     NavigationRailDestination(
+          //       icon: Icon(Icons.bookmark_border),
+          //       selectedIcon: Icon(Icons.book),
+          //       label: Text('Second'),
+          //     ),
+          //     NavigationRailDestination(
+          //       icon: Icon(Icons.star_border),
+          //       selectedIcon: Icon(Icons.star),
+          //       label: Text('Third'),
+          //     ),
+          //   ],
+          // ),
+          // VerticalDivider(thickness: 1, width: 1),
           // This is the main content.
           Expanded(
               child: Container(
+            padding: const EdgeInsets.all(15.0),
+            alignment: Alignment.center,
             decoration: BoxDecoration(
                 gradient: LinearGradient(
                     begin: Alignment.topLeft,
